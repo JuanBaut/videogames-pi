aa1d6744298a1a5989d01392ab4d24becf72c2dc
JuanBaut <deserd@protonmail.com>
2023-12-22 21:34:53 -0500

    models created

--- server/src/models/videogameModel.cjs
+++ server/src/models/videogameModel.cjs
@@ -1,9 +1,39 @@
 const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize) => {
-  sequelize.define("videogame", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Games",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      releaseDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false },
+  );
 };
